d4acccfb19cd1fbc50f796eb441b44baa97cccba
Jerry <jencisollallahui@gmail.com>
2026-07-31 23:03:55 -0500

    feat(cliente): implementar actualización de datos básicos del cliente

--- src/main/java/com/jerry/empresa/service/ClienteService.java
+++ src/main/java/com/jerry/empresa/service/ClienteService.java
@@ -9,12 +9,29 @@ import java.util.Optional;
 
 @Service
 public class ClienteService {
+    private final ClienteRepository clienteRepository;
+    public ClienteService(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
 
-public List<Cliente> getAllClientes(){
-    return clienteRepository.findAll();
-}
-}
-public Cliente getClienteById(Long id){
-    return clienteRepository.findById(id)
-            .orElseThrow(() -> new RuntimeException("Cliente no encontrado: "+id));
+    public List<Cliente> getAllClientes() {
+        return clienteRepository.findAll();
+    }
+
+    public Cliente getClienteById(Long id) {
+        return clienteRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Cliente no encontrado: " + id));
+    }
+    public Cliente createCliente(Cliente cliente){
+        return clienteRepository.save(cliente);
+    }
+    public Cliente upadteCliente(Long id, Cliente clienteDetails){
+        Cliente cliente = getClienteById(id);
+        cliente.setNombre(clienteDetails.getNombre());
+        cliente.setEmail(clienteDetails.getEmail());
+        return clienteRepository.save(cliente);
+
+
+
+    }
 }
