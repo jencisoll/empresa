b2f6d32d7b452c1e882e9d9b392240ce669be399
Jerry <jencisollallahui@gmail.com>
2026-08-01 23:25:45 -0500

    feat:add ClienteController with REST endpoints

--- src/main/java/com/jerry/empresa/controller/ClienteController.java
+++ src/main/java/com/jerry/empresa/controller/ClienteController.java
@@ -7,9 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 @RestController
 @RequestMapping("/api/clientes")
 public class ClienteController {
@@ -22,8 +19,22 @@ public class ClienteController {
     public ResponseEntity<List<Cliente>> getAllClientes(){
         return ResponseEntity.ok(clienteService.getAllClientes());
     }
-    @GetMapping("/{id")
+    @GetMapping("/{id}")
     public ResponseEntity<Cliente> getClientesById(@PathVariable Long id){
         return ResponseEntity.ok(clienteService.getClienteById(id));
+    }
+    @PostMapping
+    public ResponseEntity<Cliente> createClientesById(@RequestBody Cliente cliente){
+        return ResponseEntity.status(HttpStatus.CREATED).body(clienteService.createCliente(cliente));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente>updateCliente(@PathVariable Long id, @RequestBody Cliente cliente){
+        return ResponseEntity.ok(clienteService.updateCliente(id, cliente));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCliente(@PathVariable Long id){
+        clienteService.deleteCliente(id);
+        return ResponseEntity.noContent().build();
     }
 }
